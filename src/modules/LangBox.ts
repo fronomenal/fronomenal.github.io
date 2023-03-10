82e4903b31a68bfce6de8d92f46d78183d8bf737
fronomenal <94882375+fronomenal@users.noreply.github.com>
2023-03-10 23:57:39 +0000

    implement language mastery circle

--- src/modules/LangBox.ts
+++ src/modules/LangBox.ts
@@ -1,22 +1,29 @@
 import jQuery from "jquery";
+import CircleProgress from "js-circle-progress";
+
+const languageProficiency = new Map<string, number>();
 
 export default class LangBox
 {
     imglinks: JQuery<HTMLAnchorElement>;
+    detailLinks: JQuery<HTMLAnchorElement>;
+    langProf: JQuery<HTMLDivElement>;
     genModal: JQuery<HTMLDivElement>;
     closeBtn: JQuery<HTMLElement>;
-    detailLinks: JQuery<HTMLAnchorElement>;
 
     body: JQuery<HTMLElement>;
+    circProg: CircleProgress;
 
 
     constructor(){
         this.detailLinks = jQuery("a.details");
         this.imglinks = jQuery("a.prePhoto");
+        this.langProf = jQuery('.lang-proficiency');
         this.genModal = jQuery("<div></div>", {"class":"cus-modal"});
         this.closeBtn = jQuery('<i></i>', {"class": "fas fa-times cus-close-btn"});
 
         this.body = jQuery("body");
+        this.circProg = new CircleProgress(".pf-progress", {value: 0, max: 100, textFormat: "percent"});
     }
 
     public registerEvents(){
@@ -35,6 +42,37 @@ export default class LangBox
 
     private toggleLangDetails(event: any){
       event.preventDefault();
+      this.setProfMap();
+      this.assignProDivValues(event.target);
+      this.langProf.fadeIn();
+    }
+
+    private setProfMap(){
+      this.detailLinks.each((i, ele) => {
+        if(ele.dataset.lang && i <= 2){
+          languageProficiency.set(ele.dataset.lang, 50)
+        }else if(ele.dataset.lang && i <= 7){
+          if(i > 5) languageProficiency.set(ele.dataset.lang, 75)
+          else languageProficiency.set(ele.dataset.lang, 65)
+        }else if(ele.dataset.lang && i >= 8){
+          languageProficiency.set(ele.dataset.lang, 90)
+        }
+      });
+    }
+
+    private assignProDivValues(tg: HTMLAnchorElement){
+      const lang = tg.dataset.lang as string;
+      this.langProf.find('h4').text(lang);
+      const proficiency = languageProficiency.get(lang);
+      if (proficiency) {
+        this.langProf.find('.pf-progress').data("progress", proficiency);
+
+        if (proficiency >= 90) this.langProf.find('.pf-level').text("Proficiency: Senior")
+        else if (proficiency >= 65) this.langProf.find('.pf-level').text("Proficiency: Intermediate")
+        else this.langProf.find('.pf-level').text("Proficiency: Junior");
+
+        this.circProg.value = proficiency;
+      }
     }
 
     private createImgModal(src : string){
